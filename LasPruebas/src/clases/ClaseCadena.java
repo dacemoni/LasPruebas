5f07b4516f77adec67815c21a90061d66587590b
dacemoni <davidmoro96@gmail.com>
2022-04-04 18:08:12 +0200

    codigo

--- LasPruebas/src/clases/ClaseCadena.java
+++ LasPruebas/src/clases/ClaseCadena.java
@@ -2,4 +2,17 @@ package clases;
 
 public class ClaseCadena {
 
+    public static void DevuelveIniciales(String nombreApellidos) {
+        StringBuffer iniciales = new StringBuffer();
+        if (nombreApellidos == null || nombreApellidos.equals("")) {
+            iniciales.append("");
+        } else {
+            for (int i = 0; i < nombreApellidos.length(); i++) {
+                if (Character.isUpperCase(nombreApellidos.charAt(i))) {
+                    iniciales.append(nombreApellidos.charAt(i) + ".");
+                }
+            }
+            System.out.println(iniciales);
+        }
+    }
 }
---
+++ LasPruebas/src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import clases.ClaseCadena;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ClaseCadena.DevuelveIniciales("David Moro");
+    }
+}
